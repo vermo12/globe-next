df3c50e3139015d80c1fa880c99d9a398d775ac5
Francois Vermaut <fvermaut@gmail.com>
2021-11-27 13:47:21 +0100

    mock DWC

--- components/Fragment/GenericFragment.js
+++ components/Fragment/GenericFragment.js
@@ -1,15 +1,20 @@
 import ArticleFragment from "./ArticleFragment";
+import WebContainerFragment from "./WebContainerFragment";
 
 export default function GenericFragment({objectData, linkData}) {
     const type = objectData.sys.type;
-
     let render = null;
 
     switch (type) {
         case "article": 
             render = <ArticleFragment objectData={objectData} linkData={linkData}/>;
             break;
+        case "box":
+            render = <WebContainerFragment objectData={objectData} linkData={linkData}/>;
+            break;
         default:
+            console.log("----")
+            console.log(objectData);
             render = <p>No template found!</p>;
 
     }

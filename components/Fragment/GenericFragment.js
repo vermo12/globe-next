d8bd5e41a32a241e5eded852952b95a8425afe1e
Francois Vermaut <francois.vermaut@eidosmedia.com>
2022-02-28 17:10:03 +0100

    fix error mgmt

--- components/Fragment/GenericFragment.js
+++ components/Fragment/GenericFragment.js
@@ -3,40 +3,45 @@ import ArticleFragmentDefault from "./ArticleFragmentDefault";
 import ArticleFragmentBox from "./ArticleFragmentBox";
 import WebContainerFragment from "./WebContainerFragment";
 
-export default function GenericFragment({cobaltData,index}) {
-   
-    const baseType = cobaltData.object.data.sys.baseType;
-    const linkZone = cobaltData.linkContext.linkData.zone;
-    const linkTemplate = cobaltData.linkContext.linkData.template;
+export default function GenericFragment({ cobaltData, index }) {
+
     let render = null;
 
-    switch (baseType) {
-        case "article":
-            if (!linkTemplate){
-                switch(linkZone) {
-                    case "main":
-                    case "header":
-                    case "footer":
-                        render = <ArticleFragmentDefault cobaltData={cobaltData} index={index}/>;
-                        break;
-                    case "box":
-                        render = <ArticleFragmentBox cobaltData={cobaltData}/>;
-                        break;
-                }
-            } else {
-                switch(linkTemplate) {
-                    case "1col_big":
-                        render = <ArticleFragment1colBig cobaltData={cobaltData}/>
-                        break;
-                }
-            }      
-            break;
-        case "webpagefragment":
-            render = <WebContainerFragment cobaltData={cobaltData}/>;
-            break;
-        default:
-            render = <p>No template found! {baseType + "/" + linkZone + "/" + linkTemplate}</p>;
+    try {
 
-    }
+        const baseType = cobaltData.object.data.sys.baseType;
+        const linkZone = cobaltData.linkContext.linkData.zone;
+        const linkTemplate = cobaltData.linkContext.linkData.template;
+
+
+        switch (baseType) {
+            case "article":
+                if (!linkTemplate) {
+                    switch (linkZone) {
+                        case "main":
+                        case "header":
+                        case "footer":
+                            render = <ArticleFragmentDefault cobaltData={cobaltData} index={index} />;
+                            break;
+                        case "box":
+                            render = <ArticleFragmentBox cobaltData={cobaltData} />;
+                            break;
+                    }
+                } else {
+                    switch (linkTemplate) {
+                        case "1col_big":
+                            render = <ArticleFragment1colBig cobaltData={cobaltData} />
+                            break;
+                    }
+                }
+                break;
+            case "webpagefragment":
+                render = <WebContainerFragment cobaltData={cobaltData} />;
+                break;
+            default:
+                render = <p>No template found! {baseType + "/" + linkZone + "/" + linkTemplate}</p>;
+
+        }
+    } catch (e) { console.log(e) }
     return render;
 }
